56ba9e6b72b63f1fcd92999641b61534cd4d928b
Nisa Sabyan <mazynaila@gmail.com>
2026-02-10 15:10:10 +0700

    Update card-dimensions.ts

--- client/src/lib/card-dimensions.ts
+++ client/src/lib/card-dimensions.ts
@@ -4,13 +4,11 @@ export const ID1_CARD_HEIGHT_MM = 53.98;
 export const ID1_CORNER_RADIUS_MM = 3.18;
 export const ID1_CARD_ASPECT_RATIO = ID1_CARD_WIDTH_MM / ID1_CARD_HEIGHT_MM;
 
-export const CARD_PREVIEW_WIDTH_PX = 540;
-export const CARD_PREVIEW_HEIGHT_PX = Number(
-  (CARD_PREVIEW_WIDTH_PX / ID1_CARD_ASPECT_RATIO).toFixed(2),
-);
-export const CARD_PREVIEW_RADIUS_PX = Number(
-  ((ID1_CORNER_RADIUS_MM / ID1_CARD_HEIGHT_MM) * CARD_PREVIEW_HEIGHT_PX).toFixed(2),
-);
+
+export const CARD_PREVIEW_WIDTH_PX = 1011;
+export const CARD_PREVIEW_HEIGHT_PX = 639;
+export const CARD_PREVIEW_RADIUS_PX = 15;
+
 
 export const ID1_EXPORT_DPI = 300;
 export const ID1_EXPORT_WIDTH_PX = Math.round((ID1_CARD_WIDTH_MM / 25.4) * ID1_EXPORT_DPI);
